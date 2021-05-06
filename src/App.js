19ef3199a2e69aed361c2795d70052b0eb6af1d3
David <davidramirez9210@gmail.com>
2021-05-06 16:27:32 -0300

    useEffect to call the pixabay & save in the App state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ function App() {
   //state de la app
   const[busqueda,guardarBusqueda] = useState('');
 
-  
+  useEffect(() => {
+    const consultarApi = async () => {
+      if(busqueda === '') return;
+
+    const imagenesPorPagina = 30;
+    const key = "21500840-40c7419a84e9891fcbdfdd4a2";
+    const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+
+    guardarBusqueda(resultado.hits);
+    }
+    consultarApi();
+  },[busqueda])
 
   return (
    <div className="container">
